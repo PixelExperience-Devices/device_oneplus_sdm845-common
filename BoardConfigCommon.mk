25ca384fc968704572435ceade698dc03a4f86bc
Martinusbe <martinusbe@gmail.com>
2017-07-07 09:32:02 +0200

    sdm845-common: lockscreen charging info
    real time values

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 # TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno630
