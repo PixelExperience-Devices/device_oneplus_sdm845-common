4842b350daaa62306bd485573a5992808df68808
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-09 20:22:19 -0200

    sdm845-common: overlay: Define config_customDozePackage
    * Remove lineage leftovers
    
    Change-Id: I2a73aa85745c5f30d85675f34da49aeda5aa0773
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: yashsonawane <yash050219991@gmail.com>

--- common.mk
+++ common.mk
@@ -23,6 +23,7 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp 
 
 PRODUCT_PACKAGES += \
     OnePlusIconShapeCircleOverlay \
