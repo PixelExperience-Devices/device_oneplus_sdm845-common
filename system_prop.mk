ca1fa13258987c02ce9586d341730bfe45c86d47
darkobas <darkobas@users.noreply.github.com>
2018-08-06 01:59:25 +0200

    sdm845-common:camera hal1
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -24,7 +24,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=true
+    camera.disable_zsl_mode=true \
+    camera.hal1.packagelist=com.oneplus.camera \
+    persist.sys.camera.preview.size=1920x1080
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
