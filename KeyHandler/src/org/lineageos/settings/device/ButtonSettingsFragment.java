7b7e65b0f7932d5a8557db718a0e3151034dbddd
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:46:45 +0200

    sdm845-common: Add local FileUtils
    * We don't really need lineageos sdk for one class
    
    Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7

--- KeyHandler/src/org/lineageos/settings/device/ButtonSettingsFragment.java
+++ KeyHandler/src/org/lineageos/settings/device/ButtonSettingsFragment.java
@@ -27,7 +27,6 @@ import android.support.v7.preference.PreferenceGroup;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
-import com.android.internal.util.aosip.FileUtils;
 import org.lineageos.settings.device.utils.Constants;
 
 public class ButtonSettingsFragment extends PreferenceFragment
