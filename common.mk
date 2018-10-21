f5af7ccf478878a77e53ef9059a2975e3bbdf77f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-21 16:39:21 -0300

    sdm845-common: More cleanup
    * Cleanup unused overlays
    * Remove keyhandler
    * Adapt some overlays
    * Drop lineagehw

--- common.mk
+++ common.mk
@@ -19,8 +19,7 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
@@ -122,7 +121,6 @@ PRODUCT_COPY_FILES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Update engine
