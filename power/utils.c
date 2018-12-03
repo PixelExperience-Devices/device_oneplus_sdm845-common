7d0d1b759a9eaeba657da954c435ba77f35a045c
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-12-03 20:18:32 +0530

    power: Kill "Failed to acquire lock" log
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- power/utils.c
+++ power/utils.c
@@ -203,7 +203,7 @@ void interaction(int duration, int num_args, int opt_list[]) {
     if (qcopt_handle) {
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
-            if (lock_handle == -1) ALOGE("Failed to acquire lock.");
+            //if (lock_handle == -1) ALOGE("Failed to acquire lock.");
         }
     }
 #endif
@@ -215,7 +215,7 @@ int interaction_with_handle(int lock_handle, int duration, int num_args, int opt
     if (qcopt_handle) {
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
-            if (lock_handle == -1) ALOGE("Failed to acquire lock.");
+            //if (lock_handle == -1) ALOGE("Failed to acquire lock.");
         }
     }
     return lock_handle;
@@ -231,8 +231,8 @@ int perf_hint_enable(int hint_id, int duration) {
     if (qcopt_handle) {
         if (perf_hint) {
             lock_handle = perf_hint(hint_id, NULL, duration, -1);
-            if (lock_handle == -1)
-                ALOGE("Failed to acquire lock for hint_id: %X.", hint_id);
+           /*if (lock_handle == -1)
+                ALOGE("Failed to acquire lock for hint_id: %X.", hint_id);*/
         }
     }
     return lock_handle;
@@ -245,7 +245,7 @@ int perf_hint_enable_with_type(int hint_id, int duration, int type) {
     if (qcopt_handle) {
         if (perf_hint) {
             lock_handle = perf_hint(hint_id, NULL, duration, type);
-            if (lock_handle == -1) ALOGE("Failed to acquire lock.");
+            //if (lock_handle == -1) ALOGE("Failed to acquire lock.");
         }
     }
     return lock_handle;
@@ -262,7 +262,7 @@ void perform_hint_action(int hint_id, int resource_values[], int num_resources) 
             int lock_handle = perf_lock_acq(0, 0, resource_values, num_resources);
 
             if (lock_handle == -1) {
-                ALOGE("Failed to acquire lock.");
+                //ALOGE("Failed to acquire lock.");
             } else {
                 /* Add this handle to our internal hint-list. */
                 struct hint_data* new_hint = (struct hint_data*)malloc(sizeof(struct hint_data));
