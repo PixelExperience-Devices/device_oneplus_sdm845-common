5ac538ee686cad71e355ab1b343bdd7a370c7e94
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-12-13 08:04:19 +0000

    sdm845-common: BoardConfigCommon: Commonize defconfig
    kronic_defconfig has support for both OnePlus 6 and the OnePlus 6T
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
+TARGET_KERNEL_CONFIG := kronic_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
