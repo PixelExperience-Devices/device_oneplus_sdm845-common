0c9f56b1ba6539c0faeb7557a788c4000d992d3c
millerliang <millerliang@google.com>
2018-12-17 15:47:24 +0800

    sdm845-common: system_prop: Refined early SF phase offsets
    Sometimes SW_VSNC of DispSync comes 1ms earlier than signal_retire_fence(or HW_VSYNC)
    It means VSYNC-sf only 5ms behind signal_retire_fence.
    And VSYNC-sf will be 5.5ms earlier when applying early_sf_phase.
    (the phase-sf changes from 6ms to 0.5ms)
    
    Therefore, VSYNC-sf comes 0.5ms earlier than signal_retire_fence when
    applying early_sf_phase for transaction and cause frame missed.
    
    Add more 1ms to early_sf_phase then it will come 0.5 later than signal_retire_fence
    in this case.
    
    Bug: 118606025
    Test: Open/close apps, observe no jank
    Test: -Launch app, stay the same picture on screen at least 30s
          -Tap home key, observe no jank when doing app animation
    
    Change-Id: I2da26e844cfc3dc86d7617e44906230ded760d7c
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -32,10 +32,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
     persist.vendor.color.matrix=2 \
-    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_phase_offset_ns=500000
+    debug.sf.early_phase_offset_ns=1500000
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
