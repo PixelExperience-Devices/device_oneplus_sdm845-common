548ce3c75ebbb6ebd70725b6edddd367b701c523
Akash <akashniki@gmail.com>
2019-01-30 07:00:56 +0000

    sdm845-common:PixelExperience Bringup!

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
