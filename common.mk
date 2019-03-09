ac7ec5034946259c03b5a14beb569d8e6483778c
Akash <akashniki@gmail.com>
2019-03-09 08:00:50 +0000

    sdm845-common:Add latest RZ-changes These changes were taken from here: https://github.com/anirudhgupta109/anykernel2/blob/master/patch/init.target.rc

--- common.mk
+++ common.mk
@@ -65,7 +65,8 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
-    init.recovery.qcom.rc
+    init.recovery.qcom.rc \
+    init.prelude.rc
 
 # Display
 PRODUCT_PACKAGES += \
