80fb026e67d6b91a21938c0172b059df30ea1615
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-13 16:41:34 -0300

    sdm845-common: Enable TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,5 +136,8 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
+# Telephony
+TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
+
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
