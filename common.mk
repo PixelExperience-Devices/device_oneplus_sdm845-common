f1e387414257db10bcf22b35974bf3950215d44c
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-11-27 23:22:39 -0200

    sdm845-common: Import msm irqbalance
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- common.mk
+++ common.mk
@@ -301,6 +301,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
