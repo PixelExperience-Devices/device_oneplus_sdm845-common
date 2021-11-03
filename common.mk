6811b47b98e5144a974e37d83f6ab5fd28f0a6b9
J0SH1X <aljoshua.hell@gmail.com>
2021-11-03 12:03:02 +0100

    feat(Bootjars): Disable Telephony-ext

--- common.mk
+++ common.mk
@@ -466,8 +466,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Touch
 PRODUCT_PACKAGES += \
