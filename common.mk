9ad09e9a4553bb155514322ad930c21132774066
Arian <arian.kulmer@web.de>
2021-11-21 15:23:38 +0100

    sdm845-common: rootdir: Remove unnecessary early_boot script
    Change-Id: I8929b6d1686eb7a0600c99125665809d61cd2290

--- common.mk
+++ common.mk
@@ -181,7 +181,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
     init.oem.rc \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
