aa701d1d0455fc18ef455ec929b5e1d38aabae53
jabashque <jabashque@gmail.com>
2022-06-24 23:49:25 -0700

    sdm845-common: rootdir: Switch to zstd for zram
    Change-Id: Ie49d9e06a205bafbb705af9c5adcb08a34706a12

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -837,9 +837,10 @@ function configure_zram_parameters() {
         let zRamSizeMB=4096
     fi
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+    #if [ "$low_ram" == "true" ]; then
+        #echo lz4 > /sys/block/zram0/comp_algorithm
+    #fi
+    echo zstd > /sys/block/zram0/comp_algorithm
 
     if [ -f /sys/block/zram0/disksize ]; then
         if [ -f /sys/block/zram0/use_dedup ]; then
