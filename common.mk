04440dd62ebed1277e9f481a3aaaf835592090bd
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-11-01 11:19:18 -0700

    sdm845-common: Add lineage Aperture

--- common.mk
+++ common.mk
@@ -190,6 +190,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Aperture \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
