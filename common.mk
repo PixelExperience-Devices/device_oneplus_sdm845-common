d4dbcb38d8dc091f6c9c218367a54db8f59473b5
Joker-V2 <nipinna0@gmail.com>
2022-11-22 15:50:35 +0000

    sdm845-common: Build android.hardware.bluetooth.audio-impl
      W audiohalservice: Could not register Bluetooth Audio Offload API
      E audiohalservice: Failed to dlopen android.hardware.bluetooth.audio-impl.so: dlopen failed: library "android.hardware.bluetooth.audio-impl.so" not found
      W audiohalservice: createIBluetoothAudioProviderFactory() from android.hardware.bluetooth.audio-impl failed
    
    Change-Id: I44fa882ea93cf0e815a022d714dabe2e558031fb
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- common.mk
+++ common.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
