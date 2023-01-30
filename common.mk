e40c7216e845c74592989b606206901bcec8d68a
snnbyyds <snnbyyds@gmail.com>
2023-01-30 21:12:00 +0800

    sdm845-common: Drop livedisplay Drop livedisplay for pe standard version.

--- common.mk
+++ common.mk
@@ -312,10 +312,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oneplus_sdm845
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
