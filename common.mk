40df3480d71378e969ce622b6d205f81b4a3e935
snnbyyds <snnbyyds@gmail.com>
2023-06-23 17:39:28 +0800

    sdm845-common: rro_overlays: Drop CaptivePortalUrlOverlay

--- common.mk
+++ common.mk
@@ -179,10 +179,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-# CaptivePortalUrl
-PRODUCT_PACKAGES += \
-    CaptivePortalUrlOverlay
-
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
