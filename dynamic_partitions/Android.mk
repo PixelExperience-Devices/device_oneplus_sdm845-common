a35e2ca563013ffbcc881871ae9d9e05e3726938
snnbyyds <snnbyyds@gmail.com>
2023-07-03 13:28:57 +0800

    sdm845-common: Move to check_dynamic_partitions in hardware_google_pixel

--- dynamic_partitions/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE:= check_dynamic_partitions
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES := check_dynamic_partitions
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PREBUILT)
