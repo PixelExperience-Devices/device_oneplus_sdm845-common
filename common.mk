701b66761d93db26c84d1915f3ead507f9ac0a37
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-10-18 21:05:54 +0300

    sdm845-common: build libprotobuf-cpp-full-3.9.1 and ship prebuilt libprotobuf-cpp-full
    * libprotobuf-cpp-full-vendorcompat is no longer a thing

--- common.mk
+++ common.mk
@@ -396,6 +396,14 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
