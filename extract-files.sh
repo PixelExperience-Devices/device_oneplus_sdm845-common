13f54f88f24332f74e85ee6f017638a7ee3b28a9
snnbyyds <snnbyyds@gmail.com>
2023-11-07 18:58:37 +0800

    sdm845-common: Stop patching product/lib*/libdpmframework.so We no longer have these blobs now.

--- extract-files.sh
+++ extract-files.sh
@@ -60,11 +60,7 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        product/lib/libdpmframework.so)
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-            ;;
-        product/lib64/libdpmframework.so)
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
+        *)
             ;;
     esac
 }
