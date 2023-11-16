8546972a6582e30f603a062e870f1f40b5a5011e
snnbyyds <snnbyyds@gmail.com>
2023-11-16 09:05:12 +0800

    sdm845-common: Remove libcutils-v29.so libcutils-v29.so was once used for product/lib*/libdpmframework.so patching, but this is no longer needed for now. So let's drop it.
    REF: https://github.com/LineageOS/android_device_oneplus_sdm845-common/commit/dee2ca4cee9724a9e83cce466a66e4da150b9c78
    
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- common.mk
+++ common.mk
@@ -494,11 +494,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.0.vendor \
