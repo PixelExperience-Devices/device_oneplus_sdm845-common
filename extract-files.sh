4b6b279208405e3da11c66bc44f5cbdf00c9db37
snnbyyds <snnbyyds@gmail.com>
2023-12-02 09:42:55 +0800

    sdm845-common: Patch libwfdnative.so
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,7 +60,8 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        *)
+        system_ext/lib64/libwfdnative.so)
+            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
     esac
 }
